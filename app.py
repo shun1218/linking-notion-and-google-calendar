11eec8c3e597d05092f6c48e70408690060a697d
shun1218 <54743067+shun1218@users.noreply.github.com>
2022-03-06 01:45:30 +0900

    fix: name

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from googleapiclient.errors import HttpError
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-s3 = boto3.resource('s3', region_name='ap-northeast-1', aws_access_key_id=os.environ['AWS_ACCESS_KEY'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+s3 = boto3.resource('s3', region_name='ap-northeast-1', aws_access_key_id=os.environ['ACCESS_KEY'], aws_secret_access_key=os.environ['SECRET_ACCESS_KEY'])
 
 def lambda_handler(e, context):
     bucket = s3.Bucket(os.environ['S3_BUCKET_NAME'])
